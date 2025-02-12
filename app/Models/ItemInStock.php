483ad290b1ee0ce21b96b36ccaf55e7b15a9dd42
Alan José <alanjsdelima@gmail.com>
2025-02-12 14:00:31 -0300

    Adicionando recursos de ItemInStock

---
+++ app/Models/ItemInStock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemInStock extends Model
+{
+    /** @use HasFactory<\Database\Factories\ItemInStockFactory> */
+    use HasFactory;
+
+    protected $table = 'items_in_stock';
+
+    protected $fillable = [
+        'sku_code',
+        'barcode',
+        'name',
+        'description',
+        'unity_type',
+        'current_quantity',
+        'maximum_quantity',
+        'cost_price',
+        'sell_price',
+        'picture',
+        'location',
+        'is_active',
+        'company_id'
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
