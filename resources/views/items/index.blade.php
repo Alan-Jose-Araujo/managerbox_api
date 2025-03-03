81f8b18df1adb6e2f6b7207eb692b06b464a9b31
Emerson <emersonssantos321@gmail.com>
2025-03-03 08:25:23 -0300

    Alinhamento da tabela referente ao gerenciamento de estoque

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -29,39 +29,40 @@
                 <p>Nenhum item em estoque.</p>
             @else
                 <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th><span>ID</span></th>
-                            <th><span>Nome</span></th>
-                            <th><span>Quantidade</span></th>
-                            <th><span>Ações</span></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($items as $item)
-                            <tr>
-                                <td>{{ $item->id }}</td>
-                                <td>{{ $item->name }}</td>
-                                <td>{{ $item->current_quantity }}</td>
-                                <td>
-                                    <a href="{{ route('items.show', $item->id) }}" class="edit" title="Detalhes" data-toggle="tooltip">
-                                        <i class="material-icons">&#xE417;</i>
-                                    </a>
-                                    <a href="{{ route('items.edit', $item->id) }}" class="edit" title="Editar" data-toggle="tooltip">
-                                        <i class="material-icons">&#xE254;</i>
-                                    </a>
-                                    <form action="{{ route('items.destroy', $item->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Tem certeza que deseja excluir este item?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="delete" title="Excluir" data-toggle="tooltip">
-                                            <i class="material-icons">&#xE872;</i>
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    <thead>
+        <tr>
+            <th style="width: 5%;">ID</th>
+            <th style="width: 45%;">Nome</th>
+            <th style="width: 15%;">Quantidade</th>
+            <th style="width: 35%;">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->current_quantity }}</td>
+                <td>
+                    <a href="{{ route('items.show', $item->id) }}" class="edit" title="Detalhes" data-toggle="tooltip">
+                        <i class="material-icons">&#xE417;</i>
+                    </a>
+                    <a href="{{ route('items.edit', $item->id) }}" class="edit" title="Editar" data-toggle="tooltip">
+                        <i class="material-icons">&#xE254;</i>
+                    </a>
+                    <form action="{{ route('items.destroy', $item->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Tem certeza que deseja excluir este item?');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="delete" title="Excluir" data-toggle="tooltip">
+                            <i class="material-icons">&#xE872;</i>
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
             @endif
         </div>
     </div>
