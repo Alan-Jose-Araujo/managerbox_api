2288981df0828352a56b3a0456743ed58d05d3ed
Alan José <alanjsdelima@gmail.com>
2025-03-05 10:13:57 -0300

    Obtendo company_id da sessão

--- app/Http/Controllers/ItemInStockController.php
+++ app/Http/Controllers/ItemInStockController.php
@@ -87,7 +87,7 @@ class ItemInStockController extends Controller
             auth()->user()->refresh();
 
             // Obter o company_id do usuário autenticado
-            $companyId = auth()->user()->company_id;
+            $companyId = Session::get('company_id');
 
             if (!$companyId) {
                 Log::error('Erro ao cadastrar item: Usuário autenticado sem company_id.', [
