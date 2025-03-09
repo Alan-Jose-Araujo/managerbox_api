778862402752315efc25b7250f708ddf50c0ed15
Gabriel_Vidal02 <gabrielvidal1590@gmail.com>
2025-03-09 17:02:21 -0300

    Adicionando navegação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,11 @@
     <script src="{{ asset('js/scripts.js') }}"></script>
 </head>
 <body class="bg-gray-100">
+
+    @auth
+        @include('layouts.navbar')
+    @endauth
+
     <div class="container mx-auto p-8">
         @yield('content')
     </div>
